87b4860c43b743c07ddd58c34d5413d0a08d48f2
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2023-12-07 16:32:47 -0500

    add functionality to back button

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,5 +1,6 @@
 // Get element objects for the score list and the clear button
 const listEl = document.querySelector("#scores-list");
+const backButton = document.querySelector("#back-button");
 const clearButton = document.querySelector("#clear-button");
 
 // Render the stored scores upon page load
@@ -12,7 +13,14 @@ clearButton.addEventListener("click", function() {
     renderScores();
 })
 
-// Render the high scores to the page
+// Navigate to index.html upon back button click
+backButton.addEventListener("click", function() {
+    document.location = "index.html";
+})
+
+
+
+// Render the stored high scores to the page
 function renderScores() {
     let scores = JSON.parse(localStorage.getItem("scores"));
 
